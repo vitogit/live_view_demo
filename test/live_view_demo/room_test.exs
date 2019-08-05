85c853c852b74a57d4ca0aff5bbe6d968a656ec9
vitogit <vitogit@users.noreply.github.com>
2019-08-04 22:30:50 -0300

    Add room schema test

---
+++ test/live_view_demo/room_test.exs
@@ -0,0 +1,38 @@
+defmodule LiveViewDemo.RoomTest do
+  use LiveViewDemo.DataCase
+
+  alias LiveViewDemo.Room
+
+  describe "messages" do
+    alias LiveViewDemo.Message
+
+    @valid_attrs %{content: "some content", room: "some room", username: "some username"}
+    @invalid_attrs %{content: nil, room: nil, username: nil}
+
+    def message_fixture(attrs \\ %{}) do
+      {:ok, message} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Room.create_message()
+      message
+    end
+
+    test "messages/1 returns message rooms" do
+      message = message_fixture()
+      assert Room.messages(message.room) == [message]
+      assert Room.messages("other room") == []
+    end
+
+    test "create_message/1 with valid data creates a message" do
+      assert {:ok, %Message{} = message} = Room.create_message(@valid_attrs)
+      assert message.content == "some content"
+      assert message.room == "some room"
+      assert message.username == "some username"
+    end
+
+    test "create_message/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Room.create_message(@invalid_attrs)
+    end
+
+  end
+end
